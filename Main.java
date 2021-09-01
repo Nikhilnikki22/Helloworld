009516a7fed2dc4db659e084ea026a85fb5971b3
Nikhilnikki22 <89537941+Nikhilnikki22@users.noreply.github.com>
2021-09-01 14:52:18 -0500

    asdfasd

---
+++ Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MaddiNlab1;
+
+/**
+ *
+ * @author s543653
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a= 35,b=5,sum,sub,div,mul;
+        sum= a+b;
+        sub =a-b;
+        mul =a*b;
+        div =a/b;
+        
+        
+        System.out.println("sum="+sum);
+        System.out.println("sub="+sub);
+        System.out.println("div="+div);
+        System.out.println("mul="+mul);
+    }
+    
+}
